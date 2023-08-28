820b61d2ffeaa51cc548a1f3322aa5b6acfeb5e8
edwa0003 <edwa0003@student.monash.edu>
2023-08-28 17:59:25 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,241 @@
+/**
+ * Inside this file you will use the classes and functions from rx.js
+ * to add visuals to the svg element in index.html, animate them, and make them interactive.
+ *
+ * Study and complete the tasks in observable exercises first to get ideas.
+ *
+ * Course Notes showing Asteroids in FRP: https://tgdwyer.github.io/asteroids/
+ *
+ * You will be marked on your functional programming style
+ * as well as the functionality that you implement.
+ *
+ * Document your code!
+ */
+
+import "./style.css";
+
+import { fromEvent, interval, merge } from "rxjs";
+import { map, filter, scan } from "rxjs/operators";
+
+/** Constants */
+
+const Viewport = {
+  CANVAS_WIDTH: 200,
+  CANVAS_HEIGHT: 400,
+  PREVIEW_WIDTH: 160,
+  PREVIEW_HEIGHT: 80,
+} as const;
+
+const Constants = {
+  TICK_RATE_MS: 500,
+  GRID_WIDTH: 10,
+  GRID_HEIGHT: 20,
+} as const;
+
+const Block = {
+  WIDTH: Viewport.CANVAS_WIDTH / Constants.GRID_WIDTH,
+  HEIGHT: Viewport.CANVAS_HEIGHT / Constants.GRID_HEIGHT,
+};
+
+/** User input */
+
+type Key = "KeyS" | "KeyA" | "KeyD";
+
+type Event = "keydown" | "keyup" | "keypress";
+
+/** Utility functions */
+/**
+ * Generates a random Tetris shape that starts touching the top edge and is within the boundaries.
+ *
+ * @param width Width of the game board (number of columns)
+ * @returns Array of coordinates representing the Tetris shape
+ */
+function generateRandomTetrisShape(width) {
+  // Define Tetris shapes as an array of arrays of relative coordinates
+  const tetrisShapes = [
+    // I-shape
+    [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }],
+    // L-shape
+    [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 1, y: 2 }],
+    // J-shape
+    [{ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }, { x: 0, y: 2 }],
+    // O-shape
+    [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 0 }, { x: 1, y: 1 }],
+    // T-shape
+    [{ x: 1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }],
+    // S-shape
+    [{ x: 1, y: 0 }, { x: 2, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 }],
+    // Z-shape
+    [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 1 }],
+  ];
+
+  // Select a random Tetris shape
+  const randomShape = tetrisShapes[Math.floor(Math.random() * tetrisShapes.length)];
+
+  // Calculate a random starting column within the boundaries
+  const startX = Math.floor(Math.random() * (width - 1 - randomShape[0].x));
+
+  // Calculate the absolute coordinates of the Tetris shape
+  const absoluteShape = randomShape.map(({ x, y }) => ({
+    x: startX + x,
+    y: y,
+  }));
+
+  return absoluteShape;
+}
+
+/** State processing */
+
+type State = Readonly<{
+  gameEnd: boolean;
+}>;
+
+const initialState: State = {
+  gameEnd: false,
+} as const;
+
+/**
+ * Updates the state by proceeding with one time step.
+ *
+ * @param s Current state
+ * @returns Updated state
+ */
+const tick = (s: State) => s;
+
+/** Rendering (side effects) */
+
+/**
+ * Displays a SVG element on the canvas. Brings to foreground.
+ * @param elem SVG element to display
+ */
+const show = (elem: SVGGraphicsElement) => {
+  elem.setAttribute("visibility", "visible");
+  elem.parentNode!.appendChild(elem);
+};
+
+/**
+ * Hides a SVG element on the canvas.
+ * @param elem SVG element to hide
+ */
+const hide = (elem: SVGGraphicsElement) =>
+  elem.setAttribute("visibility", "hidden");
+
+/**
+ * Creates an SVG element with the given properties.
+ *
+ * See https://developer.mozilla.org/en-US/docs/Web/SVG/Element for valid
+ * element names and properties.
+ *
+ * @param namespace Namespace of the SVG element
+ * @param name SVGElement name
+ * @param props Properties to set on the SVG element
+ * @returns SVG element
+ */
+const createSvgElement = (
+  namespace: string | null,
+  name: string,
+  props: Record<string, string> = {}
+) => {
+  const elem = document.createElementNS(namespace, name) as SVGElement;
+  Object.entries(props).forEach(([k, v]) => elem.setAttribute(k, v));
+  return elem;
+};
+
+/**
+ * This is the function called on page load. Your main game loop
+ * should be called here.
+ */
+export function main() {
+  // Canvas elements
+  const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
+    HTMLElement;
+  const preview = document.querySelector("#svgPreview") as SVGGraphicsElement &
+    HTMLElement;
+  const gameover = document.querySelector("#gameOver") as SVGGraphicsElement &
+    HTMLElement;
+  const container = document.querySelector("#main") as HTMLElement;
+
+  svg.setAttribute("height", `${Viewport.CANVAS_HEIGHT}`);
+  svg.setAttribute("width", `${Viewport.CANVAS_WIDTH}`);
+  preview.setAttribute("height", `${Viewport.PREVIEW_HEIGHT}`);
+  preview.setAttribute("width", `${Viewport.PREVIEW_WIDTH}`);
+
+  // Text fields
+  const levelText = document.querySelector("#levelText") as HTMLElement;
+  const scoreText = document.querySelector("#scoreText") as HTMLElement;
+  const highScoreText = document.querySelector("#highScoreText") as HTMLElement;
+
+  /** User input */
+
+  const key$ = fromEvent<KeyboardEvent>(document, "keypress");
+
+  const fromKey = (keyCode: Key) =>
+    key$.pipe(filter(({ code }) => code === keyCode));
+
+  const left$ = fromKey("KeyA");
+  const right$ = fromKey("KeyD");
+  const down$ = fromKey("KeyS");
+
+  /** Observables */
+
+  /** Determines the rate of time steps */
+  const tick$ = interval(Constants.TICK_RATE_MS);
+
+  /**
+   * Renders the current state to the canvas.
+   *
+   * In MVC terms, this updates the View using the Model.
+   *
+   * @param s Current state
+   */
+  const render = (s: State, tetrisShape) => {
+    // Clear the canvas before rendering
+  svg.innerHTML = '';
+
+  // Create an array of SVG rect elements for the Tetris shape
+  const tetrisElements = tetrisShape.map(({ x, y }) =>
+    createSvgElement(svg.namespaceURI, 'rect', {
+      height: `${Block.HEIGHT}`,
+      width: `${Block.WIDTH}`,
+      x: `${Block.WIDTH * x}`,
+      y: `${Block.HEIGHT * y}`,
+      style: 'fill: green',
+    })
+  );
+
+  // Append the generated SVG elements to the canvas
+  tetrisElements.forEach(elem => svg.appendChild(elem));
+
+    // Add a block to the preview canvas
+    const cubePreview = createSvgElement(preview.namespaceURI, "rect", {
+      height: `${Block.HEIGHT}`,
+      width: `${Block.WIDTH}`,
+      x: `${Block.WIDTH * 2}`,
+      y: `${Block.HEIGHT}`,
+      style: "fill: green",
+    });
+    preview.appendChild(cubePreview);
+  };
+
+  const tetrisShape = generateRandomTetrisShape(Constants.GRID_WIDTH);
+  render(initialState, tetrisShape);
+
+  const source$ = merge(tick$)
+    .pipe(scan((s: State) => ({ gameEnd: true }), initialState))
+    .subscribe((s: State) => {
+      render(s, tetrisShape);
+
+      if (s.gameEnd) {
+        show(gameover);
+      } else {
+        hide(gameover);
+      }
+    });
+}
+
+// The following simply runs your main function on window load.  Make sure to leave it in place.
+if (typeof window !== "undefined") {
+  window.onload = () => {
+    main();
+  };
+}
